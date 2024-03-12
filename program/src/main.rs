204da6e0d03e26d6e1ed6622e72dc415493ff16c
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2024-03-12 21:23:18 +0100

    remove comments

--- program/src/main.rs
+++ program/src/main.rs
@@ -6,20 +6,6 @@ use ttfhe::{
     glwe::GlweCiphertext,
 };
 pub fn main() {
-    // let n = sp1_zkvm::io::read::<u32>();
-    // let mut a = 0;
-    // let mut b = 1;
-    // let mut sum;
-    // for _ in 1..n {
-    //     sum = a + b;
-    //     a = b;
-    //     b = sum;
-    // }
-    // sp1_zkvm::io::write(&a);
-    // sp1_zkvm::io::write(&b);
-
-    //let (bsk_i, c_prime, c_prime_rotated): (GgswCiphertext, GlweCiphertext, GlweCiphertext) = env::read();
-
     let bsk_i = sp1_zkvm::io::read::<GgswCiphertext>();
     let c_prime = sp1_zkvm::io::read::<GlweCiphertext>();
     let c_prime_rotated = sp1_zkvm::io::read::<GlweCiphertext>();
