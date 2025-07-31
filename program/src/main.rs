f8b8d388464a60ef91fb3b210db7b8a0d1c514d9
tremblaythibaultl <tremblaythibaultl@gmail.com>
2025-07-31 12:29:01 -0400

    minor comments

--- program/src/main.rs
+++ program/src/main.rs
@@ -6,11 +6,14 @@ use ttfhe::{
     glwe::GlweCiphertext,
 };
 pub fn main() {
+    // read the input
     let bsk_i = sp1_zkvm::io::read::<GgswCiphertext>();
     let c_prime = sp1_zkvm::io::read::<GlweCiphertext>();
     let c_prime_rotated = sp1_zkvm::io::read::<GlweCiphertext>();
 
+    // compute a cmux operation (one step of the blind rotation)
     let res = cmux(&bsk_i, &c_prime, &c_prime_rotated);
 
+    // write the result
     sp1_zkvm::io::write(&res);
 }
